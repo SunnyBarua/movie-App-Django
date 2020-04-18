6ebeef06a6ed2a588d23f4a9f47394fff5743878
SunnyBarua <sunnybarua95@gmail.com>
2020-04-19 02:16:12 +0600

    made ugly home page for Movie app

---
+++ movies/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.contrib import messages
+from airtable import Airtable
+import os
+
+AIRTABLE_API_KEY="key8V0J3ads6eJy12"
+AIRTABLE_MOVIESTABLE_BASE_ID="appaWB8itTCLS2B8M"
+
+AT = Airtable(os.environ.get('AIRTABLE_MOVIESTABLE_BASE_ID',AIRTABLE_MOVIESTABLE_BASE_ID),#'Movies',
+                  'Movies',api_key=os.environ.get('AIRTABLE_API_KEY',AIRTABLE_API_KEY))
+
+# Create your views here.
+def home_page(request):
+    ##print(str(request.GET.get('query','')))
+
+    user_query=str(request.GET.get('query',''))
+    search_result=AT.get_all(formula="FIND('"+user_query.lower()+"',LOWER({Name}))")
+    stuff_for_frontend={'search_result':search_result}
+    return render(request,'movies/movies_stuff.html',stuff_for_frontend)
+
